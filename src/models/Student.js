579168beaec75632fc3a754889d05c47bac0ae5f
Boopathy-416 <boopathy1865@gmail.com>
2025-09-08 00:49:11 +0530

    export and import succes download curd finished

--- src/models/Student.js
+++ src/models/Student.js
@@ -1,15 +1,3 @@
-// import mongoose from "mongoose";
-
-// const studentSchema = new mongoose.Schema({
-//   name: { type: String, required: true },
-//   class: { type: String, required: true },
-//   gender: { type: String, enum: ["male", "female"], required: true },
-//   dob: { type: Date },
-//   photo: { type: String }
-// }, { timestamps: true });
-
-// export default mongoose.model("Student", studentSchema);
-
 
 import mongoose from "mongoose";
 
